4b8a8e6d413ad3708cc5e6bc78195f7c26a8ab99
RajatSharma4 <rajatsharma0380@gmail.com>
2024-08-04 14:10:54 +0530

    new

---
+++ Ch_3_STRINGS/Sbasic.py
@@ -0,0 +1,13 @@
+Name = "Rajat"
+
+nameshort = Name[0:3]
+
+print(nameshort)
+
+character1 = Name[1]
+
+print(character1)
+
+character2 = Name[-4:-1]
+print(Name[:4]) # same as print(Name[0:4])
+print(Name[1:]) #same as print(Name[1:5])
